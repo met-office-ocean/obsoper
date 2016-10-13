9f58980cfadc40dfab863c7e9da7136f2283acc2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2016-10-13 11:57:14 +0100

    removed skips from remaining unit tests

--- obsoper/test/test_public.py
+++ obsoper/test/test_public.py
@@ -1,38 +1,41 @@
 # pylint: disable=missing-docstring, invalid-name
 import unittest
 import pkg_resources
+
+HAS_NETCDF4 = True
 try:
     import netCDF4
 except ImportError:
-    pass
+    HAS_NETCDF4 = False
+
 import numpy as np
 import obsoper
 
 
-class TestTripolar(unittest.TestCase):
-    def setUp(self):
-        orca025_grid = pkg_resources.resource_filename("obsoper.test",
-                                                       "data/orca025_grid.nc")
-        self.grid_longitudes = self.read_variable(orca025_grid, "nav_lon")
-        self.grid_latitudes = self.read_variable(orca025_grid, "nav_lat")
+if HAS_NETCDF4:
+    class TestTripolar(unittest.TestCase):
+        def setUp(self):
+            orca025_grid = pkg_resources.resource_filename("obsoper.test",
+                                                           "data/orca025_grid.nc")
+            self.grid_longitudes = self.read_variable(orca025_grid, "nav_lon")
+            self.grid_latitudes = self.read_variable(orca025_grid, "nav_lat")
 
-        self.observed_longitudes = np.array([100])
-        self.observed_latitudes = np.array([10])
+            self.observed_longitudes = np.array([100])
+            self.observed_latitudes = np.array([10])
 
-        shape = self.grid_longitudes.T.shape
-        self.constant = 30.
-        self.constant_field = np.full(shape, self.constant)
+            shape = self.grid_longitudes.T.shape
+            self.constant = 30.
+            self.constant_field = np.full(shape, self.constant)
 
-    def read_variable(self, path, name):
-        with netCDF4.Dataset(path) as dataset:
-            return np.ma.asarray(dataset.variables[name][:])
+        def read_variable(self, path, name):
+            with netCDF4.Dataset(path) as dataset:
+                return np.ma.asarray(dataset.variables[name][:])
 
-    @unittest.skip("numpy C-API issue")
-    def test_tripolar_interpolation_given_constant_surface_field(self):
-        fixture = obsoper.Tripolar(self.grid_longitudes.T,
-                                   self.grid_latitudes.T,
-                                   self.observed_longitudes,
-                                   self.observed_latitudes)
-        result = fixture.interpolate(self.constant_field)
-        expect = np.array([self.constant])
-        np.testing.assert_array_equal(expect, result)
+        def test_tripolar_interpolation_given_constant_surface_field(self):
+            fixture = obsoper.Tripolar(self.grid_longitudes.T,
+                                       self.grid_latitudes.T,
+                                       self.observed_longitudes,
+                                       self.observed_latitudes)
+            result = fixture.interpolate(self.constant_field)
+            expect = np.array([self.constant])
+            np.testing.assert_array_equal(expect, result)
