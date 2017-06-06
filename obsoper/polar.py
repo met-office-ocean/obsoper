f1085f34a18746bcfd3877f1de9854495face6af
andrewgryan <andrew.ryan@metoffice.gov.uk>
2017-06-06 09:49:14 +0100

    added a function to get the side lengths of a polygon

--- obsoper/polar.py
+++ obsoper/polar.py
@@ -1,5 +1,23 @@
 """Polar search functions"""
+import numpy as np
 
 
 def search():
     """Search function"""
+
+
+def side_lengths(points):
+    """Polygon side lengths"""
+    n_sides = len(points)
+    lengths = []
+    for iside in range(n_sides):
+        point_1 = points[iside]
+        point_2 = points[(iside + 1) % n_sides]
+        length = distance(point_1, point_2)
+        lengths.append(length)
+    return lengths
+
+
+def distance(point_1, point_2):
+    """Cartesian distance between two points"""
+    return np.sqrt(((point_2 - point_1)**2).sum())
--- obsoper/test/test_polar.py
+++ obsoper/test/test_polar.py
@@ -1,8 +1,21 @@
 # pylint: disable=missing-docstring, invalid-name
 import unittest
+import numpy as np
 from obsoper import polar
 
 
 class TestPolarSearch(unittest.TestCase):
     def test_polar_search(self):
         polar.search()
+
+    def test_side_lengths_given_unit_segment(self):
+        points = np.array([[0, 0], [1, 0]])
+        result = polar.side_lengths(points)
+        expect = [1, 1]
+        np.testing.assert_array_almost_equal(expect, result)
+
+    def test_side_lengths_given_triangle(self):
+        points = np.array([[0, 0], [1, 0], [1, 1]])
+        result = polar.side_lengths(points)
+        expect = [1, 1, np.sqrt(2)]
+        np.testing.assert_array_almost_equal(expect, result)
