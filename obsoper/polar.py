b3e4b82d9d904fc8d7ba6845077449dba8d8b860
andrewgryan <andrew.ryan@metoffice.gov.uk>
2017-06-06 11:26:38 +0100

    explored extension of Delaunay triangulation to estimate grid

--- obsoper/polar.py
+++ obsoper/polar.py
@@ -1,9 +1,41 @@
 """Polar search functions"""
 import numpy as np
+from scipy import spatial
 
 
 def search():
     """Search function"""
+
+
+def grid_structure():
+    """Estimate grid from points"""
+
+
+def join_simplices(simplex_1, simplex_2, face_index):
+    """Join 2 simplices given face index
+
+    Insert extra index in correct position of first simplex
+
+    :return: simplex representing joined simplices
+    """
+    extra_index = list(set(simplex_2) - set(simplex_1))[0]
+    result = list(simplex_1)
+    result.insert(face_index + 1, extra_index)
+    return result
+
+
+class Grid(object):
+    """Estimate grid from points
+    
+    .. note: Uses Delaunay triangulation to search grid
+    """
+    def __init__(self, points):
+        self.points = points
+        self.triangulation = spatial.Delaunay(points)
+        self.cells = [[0, 1, 2, 3]]
+
+    def find_cell(self, position):
+        return 0
 
 
 def longest_side(points):
--- obsoper/test/test_polar.py
+++ obsoper/test/test_polar.py
@@ -1,12 +1,18 @@
 # pylint: disable=missing-docstring, invalid-name
 import unittest
 import numpy as np
+from scipy import spatial
 from obsoper import polar
 
 
 class TestPolarSearch(unittest.TestCase):
     def test_polar_search(self):
         polar.search()
+
+    def test_join_simplices(self):
+        result = polar.join_simplices([2, 3, 1], [1, 3, 0], 1)
+        expect = [2, 3, 0, 1]
+        self.assertEqual(expect, result)
 
     def test_longest_side_given_right_angle_triangle(self):
         points = np.array([[0, 0], [1, 0], [1, 1]])
@@ -25,3 +31,38 @@ class TestPolarSearch(unittest.TestCase):
         result = polar.side_lengths(points)
         expect = [1, 1, np.sqrt(2)]
         np.testing.assert_array_almost_equal(expect, result)
+
+
+class TestGrid(unittest.TestCase):
+    def setUp(self):
+        self.unit_square = np.array([[0, 0], [1, 0], [1, 1]])
+
+    def test_grid_structure(self):
+        polar.grid_structure()
+
+    def test_grid(self):
+        fixture = polar.Grid(self.unit_square)
+        result = fixture.cells
+        expect = np.array([[0, 1, 2, 3]])
+        np.testing.assert_array_equal(expect, result)
+
+    def test_grid_find_cell(self):
+        fixture = polar.Grid(self.unit_square)
+        result = fixture.find_cell([0.5, 0.5])
+        expect = 0
+        np.testing.assert_array_equal(expect, result)
+
+    def test_delaunay_given_right_angled_tringle(self):
+        points = np.array([[0, 0], [1, 0], [1, 1]])
+        fixture = spatial.Delaunay(points)
+        result = fixture.simplices
+        expect = np.array([[1, 2, 0]])
+        np.testing.assert_array_equal(expect, result)
+
+    def test_delaunay_given_unit_square(self):
+        points = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+        fixture = spatial.Delaunay(points)
+        result = fixture.simplices
+        expect = np.array([[2, 3, 1],
+                           [1, 3, 0]])
+        np.testing.assert_array_equal(expect, result)
